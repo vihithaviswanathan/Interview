2b86198d8257e0848d7a95831e07f53f878a76c4
Vihitha <vihitha0907@gmail.com>
2025-07-31 13:08:07 +0530

    Initial commit

---
+++ frontend/script.js
@@ -0,0 +1,20 @@
+// script.js
+fetch("http://127.0.0.1:3000/api/products")
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById("product-list");
+    data.forEach(product => {
+      const card = document.createElement("div");
+      card.className = "product-card";
+      card.innerHTML = `
+        <h3>${product.name}</h3>
+        <p>₹ ${product.retail_price}</p>
+        <a href="product.html?id=${product.id}">View Details</a>
+      `;
+      container.appendChild(card);
+    });
+  })
+  .catch(err => {
+    document.getElementById("product-list").innerHTML = "<p>❌ Failed to load products</p>";
+    console.error(err);
+  });
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const cors = require("cors");
 
+
 const app = express();
 const port = 3000;
 app.use(cors());
